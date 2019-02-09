fa6f94ef0f24135a3608832aec53a025b5423845
Victor Dibia <victor.dibia@gmail.com>
2019-02-08 23:34:03 -0500

    update page

--- index.js
+++ index.js
@@ -1,185 +1,343 @@
-import 'carbon-components/css/carbon-components.css'
-// import 'carbon-components/script/carbon-components.css'
-import * as tf from '@tensorflow/tfjs';
-import './main.css'
-import 'babel-polyfill';
-import "jquery"
-import { update } from '@tensorflow/tfjs-layers/dist/variables';
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
 
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
 
-let modelPromise; 
-let model = null
-let video = null
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
 
-const c = document.getElementById('canvas');
-const context = c.getContext('2d');
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
 
-video = document.getElementById("myvideo")
-video.width = 500 ;
-video.height = 400 ;
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
 
-// window.onload = () => modelPromise = cocoSsd.load();
-
-const MODEL_URL = 'hand/tensorflowjs_model.pb';
-const WEIGHTS_URL = 'hand/weights_manifest.json';
-
-
-function startVideo() {
-  navigator.mediaDevices
-    .getUserMedia({
-      audio: false,
-      video: {
-        facingMode: "user",
-        width: 600,
-        height: 500
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
       }
-    })
-    .then(stream => {
-      // console.log(video)
-      video.srcObject = stream
-      video.onloadedmetadata = () => {
-        // video.play()
-      }
-    })
-}
 
-startVideo()
-setTimeout(function () {
-//   loadModel()
-}, 2000)
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
 
-async function loadModel() {
+      var module = cache[name] = new newRequire.Module(name);
 
-  model = await tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL);
-  const cat = document.getElementById('image');
-
-  console.log("Model loaded")
-  getPredictions()
-}
-
-async function getPredictions() {
-  let timeBegin = Date.now()
-
-  const batched = tf.tidy(() => {
-    const img = tf.fromPixels(video) 
-    return img.expandDims(0)
-  })
-
-  const height = batched.shape[1]
-  const width = batched.shape[2]
-
-
-
-  model.executeAsync(batched).then(result => {
-
-    const boxes = result[0].dataSync()
-    const scores = result[1].dataSync()
-    const labels = result[2].dataSync()
-    const last = result[3].dataSync()
-
-    // console.log(boxes.length, scores.length, labels.length)
-    // clean the webgl tensors
-    batched.dispose()
-    tf.dispose(result)
-
-    const prevBackend = tf.getBackend()
-    // run post process in cpu
-    tf.setBackend('cpu')
-    const indexTensor = tf.tidy(() => {
-      const boxes2 = tf.tensor2d(boxes, [
-        result[0].shape[1],
-        result[0].shape[2]
-      ])
-      return tf.image.nonMaxSuppression(
-        boxes2,
-        scores,
-        20, // maxNumBoxes
-        0.4, // iou_threshold
-        0.7 // score_threshold
-      )
-    })
-    const indexes = indexTensor.dataSync()
-    indexTensor.dispose()
-    // restore previous backend
-    tf.setBackend(prevBackend)
-
-    // console.log(indexes, result[0].shape)
-    // console.log(scores.length, boxes.length)
-
-    const predictions = buildDetectedObjects(
-      width,
-      height,
-      boxes,
-      scores,
-      indexes,
-      labels
-    )
-
-    // console.log("Objects found", predictions.length, predictions)
-    renderPredictions(predictions)
-    let timeEnd = Date.now()
-    let timeElapsed = timeEnd - timeBegin
-    updateFPS(timeElapsed)
-    // console.log("Frame rate time", timeElapsed, 1000 / timeElapsed)
-    requestAnimationFrame(getPredictions)
-
-  })
-
-}
-
-function updateFPS(fps){
-    $("#fps").text("FPS: " + Math.round(1000 /fps) + "")
-}
-
-function renderPredictions(result) {
-  // context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  canvas.width = video.width;
-  canvas.height = video.height;
-
-  context.save();
-//   context.scale(-1, 1);
-//   context.translate(-video.width, 0);
-  context.drawImage(video, 0, 0, video.width, video.height);
-  context.restore();
-  context.drawImage(video, 0, 0);
-  context.font = '10px Arial';
-
-  // console.log('number of detections: ', result.length);
-  for (let i = 0; i < result.length; i++) {
-    context.beginPath();
-    context.rect(...result[i].bbox);
-    context.lineWidth = 1;
-    context.strokeStyle = 'red';
-    context.fillStyle = 'green';
-    context.stroke();
-    context.fillText(
-      result[i].score.toFixed(3) + ' ' + result[i].class, result[i].bbox[0],
-      result[i].bbox[1] > 10 ? result[i].bbox[1] - 5 : 10);
-  }
-}
-
-function buildDetectedObjects(width, height, boxes, scores, indexes, classes) {
-  const count = indexes.length
-  const objects = []
-  for (let i = 0; i < count; i++) {
-    const bbox = []
-    for (let j = 0; j < 4; j++) {
-      bbox[j] = boxes[indexes[i] * 4 + j]
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
     }
-    const minY = bbox[0] * height
-    const minX = bbox[1] * width
-    const maxY = bbox[2] * height
-    const maxX = bbox[3] * width
-    bbox[0] = minX
-    bbox[1] = minY
-    bbox[2] = maxX - minX
-    bbox[3] = maxY - minY
-    objects.push({
-      bbox: bbox,
-      class: classes[indexes[i]],
-      score: scores[indexes[i]]
-    })
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
   }
-  return objects
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
 }
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54134" + '/');
+
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
