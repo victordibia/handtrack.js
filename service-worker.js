2383d69bc6d29978464514c500e8274a9fe3471e
Victor Dibia <chuvidi2003@gmail.com>
2019-03-08 22:34:29 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/handtrack.js/precache-manifest.c927d0b2be9c75c628d73da552b0affd.js"
+  "/handtrack.js/precache-manifest.5bfb6795be041855218d3abd594ba07b.js"
 );
 
 workbox.clientsClaim();
