bd18473d79bcc1f5d257f956d041d4d9fcf062b8
Victor Dibia <victor.dibia@gmail.com>
2019-03-12 18:28:18 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/handtrack.js/precache-manifest.a910853f340fe61c2771ba492f7f5187.js"
+  "/handtrack.js/precache-manifest.cf5840bc612ecc99b8fe643c81cf118d.js"
 );
 
 workbox.clientsClaim();
